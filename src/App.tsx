020eb8d7873180385b4ee2e1a9d90b3134c11079
Denys Lysiak <lisyak99.dl@gmail.com>
2024-10-20 16:37:39 +0300

    Todo App

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,58 @@
-import React from 'react';
-import './App.scss';
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import { UserWarning } from './UserWarning';
+import { USER_ID } from './api/todos';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
+import { Header } from './components/Header/Header';
+import { Main } from './components/Main/Main';
+import { Footer } from './components/Footer/Footer';
+import { Errors } from './components/Errors/Errors';
+import { IsActiveLink } from './types/types';
+import { useAppDispatch, useAppSelector } from './app/hooks';
+import { loadTodos } from './app/features/todosSlice';
 
 export const App: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const { todos, hasError } = useAppSelector(state => state.todos);
+
+  const [link, setLink] = useState(IsActiveLink.All);
+
+  useEffect(() => {
+    dispatch(loadTodos());
+  }, []);
+
+  const visibleTodos = React.useMemo(() => {
+    if (link === IsActiveLink.Active) {
+      return todos.filter(t => !t.completed);
+    }
+
+    if (link === IsActiveLink.Completed) {
+      return todos.filter(t => t.completed);
+    }
+
+    return todos;
+  }, [link, todos]);
+
+  if (!USER_ID) {
+    return <UserWarning />;
+  }
+
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
+
+      <div className="todoapp__content">
+        <Header />
+
+        <Main filteredTodos={visibleTodos} />
+
+        <Footer link={link} setLink={setLink} />
+      </div>
+
+      <Errors hasError={hasError} />
     </div>
   );
 };
